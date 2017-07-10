62179c6aca1a1d7c782b3d1f745579ebd5b93fd4
edwincarbajal <exaviercarbajal@gmail.com>
2017-07-10 00:52:07 -0400

    Create search action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,19 @@
 class StaticPagesController < ApplicationController
-
   def root
   end
 
   def favorites
   end
 
+  def search
+    respond_to do |format|
+      begin
+        @gem = Gems.info params[:search]
+        rescue JSON::ParserError
+        format.js { render action: 'gemnotfound' }
+      end
+      format.html { redirect_to :back }
+      format.js
+    end
+  end
 end
